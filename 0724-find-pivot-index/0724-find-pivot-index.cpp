845956496f2697c1b0d2445eeea58456ba8ac91e
HARSH SAHU <harshsahu1229@gmail.com>
2026-03-30 06:52:43 +0530

    Time: 0 ms (100%), Space: 35.7 MB (78.51%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,42 +1,18 @@
-// class Solution {
-// public:
-//     int pivotIndex(vector<int>& nums) {
-//         int t=0;
-//         for(int x:nums){
-//             t+=x;
-//         }
-//         int l=0;
-//         for(int i=0;i<nums.size()-1;i++){
-//             int r= t-l-nums[i];
-//             if(l==r){
-//                 return i;
-//             }
-//             l=l+nums[i];
-//         }
-//         return -1;
-//     }
-// };
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int total = 0;
-        
-        for(int x : nums){
-            total += x;
+        int t=0;
+        for(int x:nums){
+            t+=x;
         }
-        
-        int left = 0;
-        
-        for(int i = 0; i < nums.size(); i++){
-            int right = total - left - nums[i];
-            
-            if(left == right){
+        int l=0;
+        for(int i=0;i<nums.size();i++){
+            int r= t-l-nums[i];
+            if(l==r){
                 return i;
             }
-            
-            left += nums[i];
+            l=l+nums[i];
         }
-        
         return -1;
     }
 };
