5a7e43079ca9218bc0a3ce5ce07712f81a6dcda0
HARSH SAHU <harshsahu1229@gmail.com>
2026-04-10 03:49:12 +0530

    Time: 0 ms (100%), Space: 14.8 MB (53.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,9 +4,9 @@ public:
         int n= nums.size();
         unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
-            int s=target-nums[i];
-            if(mp.find(s)!=mp.end()){
-                return {i,mp[s]};
+            int c= target-nums[i];
+            if(mp.find(c)!=mp.end()){
+                return {i , mp[c]};
             }else{
                 mp[nums[i]]=i;
             }
