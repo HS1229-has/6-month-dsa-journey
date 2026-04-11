cbda0bf7be40ef9e3e106c9ac3adcba4620fe191
HARSH SAHU <harshsahu1229@gmail.com>
2026-04-11 22:54:55 +0530

    Time: 4 ms (19.3%), Space: 28 MB (98.92%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,7 +3,6 @@ public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n= nums.size();
-        int x= n/2;
-        return nums[x];
+       return nums[n/2];
     }
 };
