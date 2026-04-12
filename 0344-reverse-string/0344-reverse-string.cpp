d7d6770e6b53bf4a946654f43f431f0be4d85b4e
HARSH SAHU <harshsahu1229@gmail.com>
2026-04-12 09:13:22 +0530

    Time: 0 ms (100%), Space: 27.9 MB (5.06%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        stack<char>st;   int n= s.size();
+        for(int i=0;i<n;i++){
+            st.push(s[i]);
+        }
+        int i=0;
+        while(!st.empty()){
+          s[i]=st.top();
+          st.pop();
+          i++;
+        }
+       return;
+    }
+};
