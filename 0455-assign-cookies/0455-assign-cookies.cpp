d9081fdb7efd31034b62613098fd24398641eed2
HARSH SAHU <harshsahu1229@gmail.com>
2026-05-05 00:09:40 +0530

    Time: 4 ms (85.41%), Space: 44.8 MB (46.84%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int n= g.size(); int m= s.size();
+        int i,j;
+        i=0; j=0;
+
+        while(i<n && j<m){
+if(s[j]>=g[i]){
+    i++;
+}
+j++;
+        }
+        return i;
+    }
+};
