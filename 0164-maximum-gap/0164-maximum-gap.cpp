d55f3a916707ede53ce2df1526e7537fa1e9c31d
HARSH SAHU <harshsahu1229@gmail.com>
2026-06-11 15:54:27 +0530

    Time: 40 ms (55.6%), Space: 86 MB (45.08%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if(nums.size()<=1){
+            return 0;
+        }else 
+        sort(nums.begin(),nums.end());
+        int ans= INT_MIN;
+        for(int i=1;i<nums.size();i++){
+            ans = max(ans,(nums[i]-nums[i-1]));
+
+        }
+        return ans;
+    }
+};
