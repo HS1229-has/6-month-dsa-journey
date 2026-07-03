6dedf729842690082df9daa6ae2867a966e6e9d3
HARSH SAHU <harshsahu1229@gmail.com>
2026-07-03 19:16:27 +0530

    Time: 0 ms (100%), Space: 8.7 MB (7.5%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-int summ (int num){
-    int digit;
-    
-int sum=0;
+
+int summ(int num){
+    int sum =0;
     while(num!=0){
-        digit=num%10;
-        sum = sum+digit;
-        num=num/10;
+        sum+=num%10;
+        num/=10;
     }
     return sum;
 }
     int addDigits(int num) {
-       while(num>=10){
-     num=summ(num);
-   
-    }
-      return num;
+        summ(num);
+        while(num>=10){
+            num=summ(num);
+        }
+        return num;
     }
 };
